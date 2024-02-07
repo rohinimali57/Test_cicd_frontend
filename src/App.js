e81fd0db425a1ed87e8a1bb7215a62e971a9264d
Rohini <rohinimali284@gmail.com>
2024-02-07 17:19:53 +0530

    'rohini'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1> Hello World.... </h1>
+      <h3>This is first pipeline I have created and it is working fine...</h3>
+      <h5>Thank You All.</h5>
+      <h6>From Jagruti.</h6>
+    </div>
+  );
+}
+
+export default App;
